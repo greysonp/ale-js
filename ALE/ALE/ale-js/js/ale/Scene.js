447a1946b734d6a20d547decee4eae4b96e73b0b
Greyson Parrelli <greyson.parrelli@live.com>
2013-04-30 02:20:31 -0400

    Didn't capture all of my folder structure changes last time I committed. Should be there now.

---
+++ ALE/ALE/ale-js/js/ale/Scene.js
@@ -0,0 +1,31 @@
+﻿this.ALE = this.ALE || {};
+this.box2d = this.box2d || {};
+
+(function (namespace)
+{
+    namespace.Scene = function ()
+    {
+        init();
+    }
+    var p = namespace.Scene.prototype;
+    // ===================================
+    
+    var container = {};
+
+    function init()
+    {
+        container = new createjs.Container();
+    }
+
+    p.attachChild = function (child)
+    {
+        console.log("attached Child: " + child);
+        container.addChild(child);
+    }
+
+    p.getContainer = function ()
+    {
+        return container;
+    }
+
+})(this.ALE);
