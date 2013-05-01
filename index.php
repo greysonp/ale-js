39b51b94b169170cbb44cec6488265e6b11938c9
Greyson Parrelli <greyson.parrelli@live.com>
2013-05-01 13:44:04 -0400

    Added a github link to the website.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 <h1>ale-js</h1>
 <p>This is some text about ale-js.</p>
+<p><a href="http://github.com/greysonp/ale-js">Github</a></p>
 <p><a href="web-fullscreen/index.html">Fullscreen Example</a></p>
 
 </body>
